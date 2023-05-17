f7ebc0b71087e2b3cd476ab82e52f695dfc7f295
Pheon-Dev <devpheon@gmail.com>
2023-05-17 23:14:26 +0300

    [~] Home page

--- src/components/Home.tsx
+++
@@ -1,8 +0,0 @@
-import { Text } from "@mantine/core";
-
-const Home = () => {
-  return (<>
-    <Text>Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.</Text>
-  </>)
-}
-export default Home;
