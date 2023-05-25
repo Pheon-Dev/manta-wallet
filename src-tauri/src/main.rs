4eb5ac63b8b5555489c02204bdb140072acb07e3
Pheon-Dev <devpheon@gmail.com>
2023-05-25 12:25:44 +0300

    [+] create send request api handler from manta api

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -29,6 +29,27 @@ async fn login_request(username: String, password: String) -> String {
 }
 
 #[tauri::command]
+async fn create_request(amount: String, receiver: String, description: String) -> String {
+    let url = format!("http://127.0.0.1:8080/api/create");
+    let mut map = HashMap::new();
+    map.insert("amount", amount);
+    map.insert("receiver", receiver);
+    map.insert("description", description);
+
+    let client = reqwest::Client::new();
+    let res = client
+        .post(&url)
+        .json(&map)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+    return res;
+}
+
+#[tauri::command]
 async fn list_requests() -> String {
     let url = format!("http://127.0.0.1:8080/api/send");
     let res = reqwest::get(&url).await.unwrap().text().await.unwrap();
@@ -45,6 +66,7 @@ fn main() {
         .invoke_handler(tauri::generate_handler![
             greet,
             login_request,
+            create_request,
             list_requests
         ])
         .run(tauri::generate_context!())
